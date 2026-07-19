79335e431eca6624271f53d3dd9fb9f722018bd3
Rehan <rehan.d.3011@gmail.com>
2026-07-19 12:57:00 +0530

    ✅ BIRYANI - Rating 257 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-07-19

---
+++ codechef/easy/BIRYANI---Rating-257/solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int cl, pr;
+		
+		for(int i=0; i<n; i++){
+		 cl = sc.nextInt();
+		 pr = sc.nextInt();
+		 
+		 System.out.println(cl*pr);
+		}
+
+	}
+}
