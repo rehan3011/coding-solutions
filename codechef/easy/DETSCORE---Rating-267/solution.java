3940a466dc3889cb965666b5515463403d73b550
Rehan <rehan.d.3011@gmail.com>
2026-07-19 14:57:10 +0530

    ✅ DETSCORE - Rating 267 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-07-19

---
+++ codechef/easy/DETSCORE---Rating-267/solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	        Scanner sc = new Scanner(System.in);
+	        
+	        int n = sc.nextInt();
+	        int x,y,z;
+	        for(int i=0; i<n; i++){
+	             x = sc.nextInt();
+	             y= sc.nextInt();
+	             z = (x*y)/10;
+	            System.out.println(z);
+	        }
+
+	}
+}
