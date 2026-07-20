1d9d8220f4fc1b9997292b041a797ef90eb5699b
Rehan <rehan.d.3011@gmail.com>
2026-07-20 22:30:11 +0530

    ✅ KITCHENTIME - Rating 273 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-07-20

---
+++ codechef/easy/KITCHENTIME---Rating-273/solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	        Scanner sc = new Scanner(System.in);
+	        
+	        int n = sc.nextInt();
+	        int x,y;
+	        for(int i=0; i<n; i++){
+	            x= sc.nextInt();
+	            y= sc.nextInt();
+	            
+	            System.out.println(y-x);
+	           
+	        }
+
+	}
+}
