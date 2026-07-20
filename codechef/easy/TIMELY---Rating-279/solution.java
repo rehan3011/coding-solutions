37cb66e46417559707c5dc2829fb3f926147fc7a
Rehan <rehan.d.3011@gmail.com>
2026-07-20 22:46:52 +0530

    ✅ TIMELY - Rating 279 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-07-20

--- codechef/easy/TIMELY---Rating-279/solution.java
+++ codechef/easy/TIMELY---Rating-279/solution.java
@@ -6,23 +6,21 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
-        	        Scanner sc = new Scanner(System.in);
+			        Scanner sc = new Scanner(System.in);
 	        
 	        int n = sc.nextInt();
 	        int x;
 	        
-	        for(int i=0 ;i<n; i++){
+	        for(int i=0; i<n; i++){
 	            x = sc.nextInt();
 	            
-	            if(x>=67&&x<=45000){
+	            if(x>=30){
 	                System.out.println("YES");
 	            }
 	            else{
 	                System.out.println("NO");
 	            }
 	        }
-	        
-	        
+
 	}
 }
