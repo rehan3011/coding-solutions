9832d8f4df7872f84b890370ccd83c79d04f2968
Rehan <rehan.d.3011@gmail.com>
2026-07-22 23:16:17 +0530

    ✅ ADJSUMPAR - Rating 1013 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-07-22

---
+++ codechef/easy/ADJSUMPAR---Rating-1013/solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+				Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-- >0)
+		{
+		    int n=sc.nextInt();
+		    int[] binarray=new int[n];
+		    int i,j,count=0;
+		    for(i=0;i<binarray.length;i++)
+		    {
+		        binarray[i]=sc.nextInt();
+		        if(binarray[i]==1)
+		            count++;
+		    }
+		    if(count%2==0)
+		        System.out.println("YES");
+		    else
+		        System.out.println("NO");
+		}
+
+	}
+}
