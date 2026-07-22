0149ed152562f209c0c4e9d68947e8c3b7c63b55
Rehan <rehan.d.3011@gmail.com>
2026-07-22 23:27:48 +0530

    ✅ Integer to Roman | Medium | Java | Runtime: 3 ms (99.87%) | Memory: 46.5 MB | 2026-07-22

--- leetcode/medium/0012-integer-to-roman/solution.java
+++ leetcode/medium/0012-integer-to-roman/solution.java
@@ -1,9 +1,19 @@
 class Solution {
-public static String intToRoman(int num) {
-    String M[] = {"", "M", "MM", "MMM"};
-    String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-    String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-    String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-    return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
-}
+    public String intToRoman(int num) {
+        final int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        final String[] symbols = {"M", "CM", "D",  "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < values.length; ++i) {
+            if (num == 0)
+                break;
+            while (num >= values[i]) {
+                sb.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+
+        return sb.toString();        
+    }
 }
