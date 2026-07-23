47093c8d155c6a051815acdcc5bfc37633dfcad6
Rehan <rehan.d.3011@gmail.com>
2026-07-23 22:55:20 +0530

    ✅ BNE_APT - Rating 280 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-07-23

---
+++ codechef/easy/BNE_APT---Rating-280/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		    int a = sc.nextInt();
+		    int b = sc.nextInt();
+		    int c = sc.nextInt();
+		    int d = sc.nextInt();
+		    
+		    int ans = a*c+b*d;
+		    
+		    System.out.println(ans);
+
+	}
+}
