8bd27776c5c85c24bffb027a9a0ed11adcd61161
Rehan <rehan.d.3011@gmail.com>
2026-07-23 22:39:21 +0530

    ✅ Find the Index of the First Occurrence in a String | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 43.1 MB | 2026-07-23

--- leetcode/easy/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.java
+++ leetcode/easy/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.java
@@ -1,17 +1,22 @@
 class Solution {
     public int strStr(String haystack, String needle) {
 
-        int haylength=haystack.length();
-        int needlelength=needle.length();
-        if(haylength<needlelength)
-            return -1;
-        for(int i=0;i<=haystack.length()-needle.length();i++){
-            int j=0;
-            while(j<needle.length() && haystack.charAt(i+j)==needle.charAt(j))
-                j++;
-            if(j==needle.length()){
-                return i;
-            }
+        // int haylength=haystack.length();
+        // int needlelength=needle.length();
+        // if(haylength<needlelength)
+        //     return -1;
+        // for(int i=0;i<=haystack.length()-needle.length();i++){
+        //     int j=0;
+        //     while(j<needle.length() && haystack.charAt(i+j)==needle.charAt(j))
+        //         j++;
+        //     if(j==needle.length()){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+        if(haystack.contains(needle)){
+            return haystack.indexOf(needle);
         }
         return -1;
     }
