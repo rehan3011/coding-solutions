004e8a7d878714ebcacadeeb6727f867eacc449f
Rehan <rehan.d.3011@gmail.com>
2026-07-24 19:18:34 +0530

    ✅ Array Traversal | Basic | Java | Runtime: N/A | Memory: N/A | 2026-07-24

---
+++ gfg/basic/array-traversal/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static void arrayTraversal(int[] arr) {
+        // Code here
+        int n = arr.length;
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
