3481f9fce7cfd02a1409629219956b7135044483
Rehan <rehan.d.3011@gmail.com>
2026-07-29 23:22:54 +0530

    ✅ Squares of a Sorted Array | Easy | Java | Runtime: 1 ms (100.00%) | Memory: 47 MB | 2026-07-29

--- leetcode/easy/0977-squares-of-a-sorted-array/solution.java
+++ leetcode/easy/0977-squares-of-a-sorted-array/solution.java
@@ -1,14 +1,14 @@
 class Solution {
-    public int[] sortedSquares(int[] A) {
-        int n = A.length;
+    public int[] sortedSquares(int[] arr) {
+        int n = arr.length;
         int[] result = new int[n];
         int i = 0, j = n - 1;
         for (int p = n - 1; p >= 0; p--) {
-            if (Math.abs(A[i]) > Math.abs(A[j])) {
-                result[p] = A[i] * A[i];
+            if (Math.abs(arr[i]) > Math.abs(arr[j])) {
+                result[p] = arr[i] * arr[i];
                 i++;
             } else {
-                result[p] = A[j] * A[j];
+                result[p] = arr[j] * arr[j];
                 j--;
             }
         }
