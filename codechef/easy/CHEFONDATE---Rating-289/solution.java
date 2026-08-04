be48d43690371aecc10f3a6b7ba56c6ca83306d5
Rehan <rehan.d.3011@gmail.com>
2026-08-04 23:33:29 +0530

    ✅ CHEFONDATE - Rating 289 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-04

---
+++ codechef/easy/CHEFONDATE---Rating-289/solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		for(int i = 0; i<n; i++){
+		    int k = sc.nextInt();
+		    if(k%3==0){
+		        System.out.println("YES");
+		    }
+		    else{
+		        System.out.println("NO");
+		    }
+		}
+
+	}
+}
