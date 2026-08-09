29c101fd32f9f2e114194c53aac5eb706033e8e1
Rehan <rehan.d.3011@gmail.com>
2026-08-09 13:32:57 +0530

    ✅ Swap Nodes in Pairs | Medium | Java | Runtime: 0 ms (100.00%) | Memory: 43.2 MB | 2026-02-09

--- leetcode/medium/0024-swap-nodes-in-pairs/solution.java
+++ leetcode/medium/0024-swap-nodes-in-pairs/solution.java
@@ -11,16 +11,62 @@
 class Solution {
     public ListNode swapPairs(ListNode head) {
          if(head==null||head.next==null) return head;
-        ListNode curr = head;
-       
-        while(curr!=null&&curr.next!=null){
-            int temp = curr.val;
-            curr.val = curr.next.val;
-            curr.next.val = temp;
-            curr= curr.next.next; 
-        }
 
-        return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode temp = dummy;
+
+        while(temp.next!=null&&temp.next.next!=null){
+            ListNode f = temp.next;
+            ListNode s = temp.next.next;
+
+            f.next = s.next;
+            s.next = f;
+            temp.next = s;
+
+            temp = f;
+        }
+        return dummy.next;
+
+
+        //Wrong Approach
+        // ListNode l = new ListNode(0);
+        // ListNode temp = l;
+
+        // ListNode f =head;
+        // ListNode s =head.next;
+
+        // while(s!=null&&s.next!=null){
+        //     temp.next = s;
+        //     temp.next = f;
+        //     temp = temp.next;
+        //     s = s.next.next;
+        //     f = f.next.next;
+        // }
+
+        // return l.next;
+
+        //Wrong Approach
+        // ListNode curr = head;
+        // ListNode dummy = new ListNode(0);
+        // ListNode temp = dummy;
+
+       
+        // while(curr!=null&&curr.next!=null){
+        //     int emp = curr.val;
+        //     curr.val = curr.next.val;
+        //     curr.next.val = emp;
+        //     curr= curr.next.next; 
+        // }
+        // curr = head;
+
+        // while(curr!=null){
+        //     temp.next = curr;
+        //     curr=curr.next;
+        //     temp=temp.next;
+        // }
+
+        // return dummy.next;
         
     }
 }
