9559081c0bf4aab4ea5ce5c20ed4ff1010c0e4fb
Rehan <rehan.d.3011@gmail.com>
2026-08-11 23:25:09 +0530

    ✅ AGEING - Rating 299 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-11

---
+++ codechef/easy/AGEING---Rating-299/solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		while(t>0){
+		    int a = sc.nextInt();
+		    System.out.println(a-10);
+		    t--;
+		}
+
+	}
+}
