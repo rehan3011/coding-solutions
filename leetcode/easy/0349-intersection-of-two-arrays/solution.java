fa576867e946814a05ab2e2d78a1cbf8eea17f20
Rehan <rehan.d.3011@gmail.com>
2026-08-12 23:21:38 +0530

    ✅ Intersection of Two Arrays | Easy | Java | Runtime: 2 ms (97.17%) | Memory: 44.9 MB | 2026-08-12

---
+++ leetcode/easy/0349-intersection-of-two-arrays/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set1 = new HashSet<>();
+        for (int num : nums1) {
+            set1.add(num);
+        }
+
+        HashSet<Integer> intersectionSet = new HashSet<>();
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                intersectionSet.add(num);
+            }
+        }
+
+        int[] result = new int[intersectionSet.size()];
+        int index = 0;
+        for (int num : intersectionSet) {
+            result[index++] = num;
+        }
+
+        return result;
+    }
+}
