6002c1eedde28605e24b568a963109d97e6901bf
Rehan <rehan.d.3011@gmail.com>
2026-08-13 22:24:15 +0530

    ✅ Valid Palindrome II | Easy | Java | Runtime: 4 ms (99.08%) | Memory: 47.5 MB | 2026-08-13

---
+++ leetcode/easy/0680-valid-palindrome-ii/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int n = s.length();
+        int left = 0;
+        int right = n-1;
+        int c = 0;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
+    
