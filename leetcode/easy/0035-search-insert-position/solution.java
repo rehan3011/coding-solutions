91f98286744d10f7aec3babaccc1f4a65a386741
Rehan <rehan.d.3011@gmail.com>
2026-08-14 20:12:46 +0530

    ✅ Search Insert Position | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 44.9 MB | 2026-08-14

---
+++ leetcode/easy/0035-search-insert-position/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int searchInsert(int[] arr, int target) {
+        int n = arr.length;
+
+        int l = 0;
+        int h = n-1;
+        
+
+        while(l<=h){
+            int mid = l+(h-l)/2;
+
+            if(arr[mid]==target){
+                return mid;
+            }
+            else if(arr[mid]>target){
+                h = mid-1;
+              
+            }
+            else {
+                l = mid+1;
+                
+            }
+        }
+
+        return l;
+
+
+    }
+}
