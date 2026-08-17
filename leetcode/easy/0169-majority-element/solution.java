7d9d738e270dc840ae8d3463a6847da01856dc4c
Rehan <rehan.d.3011@gmail.com>
2026-08-17 20:02:25 +0530

    ✅ Majority Element | Easy | Java | Runtime: 13 ms (32.07%) | Memory: 52.8 MB | 2026-08-17

---
+++ leetcode/easy/0169-majority-element/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+
+        HashMap <Integer, Integer> hm = new  HashMap<>();
+
+        for(int num:nums){
+            hm.put(num,hm.getOrDefault(num,0)+1);
+        }
+
+        for(int i:hm.keySet()){
+            if(hm.get(i)>n/2) return i;
+        }
+
+        return -1;
+    }
+}
