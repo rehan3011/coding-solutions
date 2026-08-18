07d8689ac740fe886d2148e3ff56ed518dfc1465
Rehan <rehan.d.3011@gmail.com>
2026-08-18 14:55:25 +0530

    ✅ Intersection of Two Linked Lists | Easy | Java | Runtime: 2 ms (48.45%) | Memory: 52.5 MB | 2026-08-18

--- leetcode/easy/0160-intersection-of-two-linked-lists/solution.java
+++ leetcode/easy/0160-intersection-of-two-linked-lists/solution.java
@@ -10,14 +10,56 @@
  * }
  */
 public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode p1 = headA;
-        ListNode p2 = headB;
-
-        while(p1!=p2){
-           p1= (p1==null)?headB:p1.next;
-           p2= (p2==null)?headA:p2.next;
+    public static int len(ListNode l){
+        int len = 0;
+        while(l!=null){
+            len++;
+            l = l.next;
         }
-        return p2;
+        return len;
     }
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+       
+       int lenA = len(headA);
+       int lenB = len(headB);
+
+       ListNode p1 = headA;
+       ListNode p2 = headB;
+
+       while(lenA>lenB){
+        p1 = p1.next;
+        lenA--;
+       }
+
+       while(lenB>lenA){
+        p2 = p2.next;
+        lenB--;
+       }
+
+       while(p1!=null||p2!=null){
+        if(p1==p2){
+            return p1;
+        }
+        p1 = p1.next;
+        p2 = p2.next;
+       }
+
+         return null;
+    }
+   
 }
+
+
+
+
+
+
+       //optimal solution
+        // ListNode p1 = headA;
+        // ListNode p2 = headB;
+
+        // while(p1!=p2){
+        //    p1= (p1==null)?headB:p1.next;
+        //    p2= (p2==null)?headA:p2.next;
+        // }
+        // return p2;
