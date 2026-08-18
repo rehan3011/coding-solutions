9b62a7a6636e11de0fe7e56864602fd07103ebe4
Rehan <rehan.d.3011@gmail.com>
2026-08-18 14:00:52 +0530

    ✅ Sort an Array | Medium | Java | Runtime: 31 ms (28.23%) | Memory: 82.8 MB | 2026-08-18

--- leetcode/medium/0912-sort-an-array/solution.java
+++ leetcode/medium/0912-sort-an-array/solution.java
@@ -1,23 +1,7 @@
 class Solution {
+    //mergesort approach
     public int[] sortArray(int[] nums) {
         
-
-        //wrong approach
-        // int n = nums.length;
-        // int freq[] = new int[n+2];
-        
-        // for(int num:nums){
-        //     freq[num]++;
-        // }
-        // int k=0;
-        // for(int j=0; j<freq.length; j++){
-        //     while(freq[j]>0){
-        //         nums[k++] = j;
-        //         freq[j]--;
-        //     }
-        // }
-        // return nums;
-
         mergeSort(nums);
         return nums;
     }
@@ -45,7 +29,6 @@ class Solution {
         mergeSort(rarr);
         merge(arr,larr,rarr);
     }
-    
     public static void merge(int[] arr, int[] larr, int[] rarr){
         int lLen = larr.length;
         int rLen = rarr.length;
@@ -76,3 +59,57 @@ class Solution {
         }
     }
 }
+
+// quicksort approach
+    
+    // public int[] sortArray(int[] nums) {
+    //     int n = nums.length;
+    //     int low = 0;
+    //     int high = n-1;
+    //     quickSort(nums, low, high);
+    //     return nums;
+    // }
+
+    // public static void quickSort(int[] arr, int low, int high){
+    //     if(low<high) {
+    //         int pivot = partition(arr, low, high);
+
+    //         quickSort(arr, low, pivot-1);
+    //         quickSort(arr,pivot+1,high);
+    //     }
+    // }
+    
+    // public static int partition(int[] arr, int low, int high){
+    //     int pivot = arr[high];
+    //     int i = low-1;
+
+    //     for(int j=low; j<high; j++){
+    //         if(arr[j]<=pivot){
+    //             i++;
+    //             int temp = arr[i];
+    //             arr[i] = arr[j];
+    //             arr[j] = temp;
+    //         }
+    //     }
+    //     int temp = arr[i+1];
+    //     arr[i+1] = arr[high];
+    //     arr[high] = temp;
+    //     return i+1;
+    // }
+
+
+        //wrong approach
+        // int n = nums.length;
+        // int freq[] = new int[n+2];
+        
+        // for(int num:nums){
+        //     freq[num]++;
+        // }
+        // int k=0;
+        // for(int j=0; j<freq.length; j++){
+        //     while(freq[j]>0){
+        //         nums[k++] = j;
+        //         freq[j]--;
+        //     }
+        // }
+        // return nums;
