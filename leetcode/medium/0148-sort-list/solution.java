6aa647b916e776cf03a82294c0aadc66705eb801
Rehan <rehan.d.3011@gmail.com>
2026-08-20 22:42:58 +0530

    ✅ Sort List | Medium | Java | Runtime: 10 ms (51.40%) | Memory: 59.4 MB | 2026-08-20

--- leetcode/medium/0148-sort-list/solution.java
+++ leetcode/medium/0148-sort-list/solution.java
@@ -8,52 +8,101 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-public class Solution {
+
+ class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode mid = getMiddleAndCut(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+        return merge(left, right);
+    }
+
+    private ListNode merge(ListNode left, ListNode right) {
+        ListNode dummy = new ListNode();
+        ListNode current = dummy;
+
+        while (left != null && right != null) {
+            if (left.val <= right.val) {
+                current.next = left;
+                left = left.next;
+            } else {
+                current.next = right;
+                right = right.next;
+            }
+
+            current = current.next;
+        }
+
+        current.next = left != null ? left : right;
+
+        return dummy.next;
+    }
+
+    private ListNode getMiddleAndCut(ListNode node) {
+        ListNode slow = node;
+        ListNode fast = node.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode mid = slow.next;
+        slow.next = null;
+
+        return mid;
+    }
+}
+// public class Solution {
   
-  public ListNode sortList(ListNode head) {
-    if (head == null || head.next == null)
-      return head;
+//   public ListNode sortList(ListNode head) {
+//     if (head == null || head.next == null)
+//       return head;
         
     
-    ListNode prev = null, slow = head, fast = head;
+//     ListNode prev = null, slow = head, fast = head;
     
-    while (fast != null && fast.next != null) {
-      prev = slow;
-      slow = slow.next;
-      fast = fast.next.next;
-    }
+//     while (fast != null && fast.next != null) {
+//       prev = slow;
+//       slow = slow.next;
+//       fast = fast.next.next;
+//     }
     
-    prev.next = null;
-    
-    
-    ListNode l1 = sortList(head);
-    ListNode l2 = sortList(slow);
+//     prev.next = null;
     
     
-    return merge(l1, l2);
-  }
+//     ListNode l1 = sortList(head);
+//     ListNode l2 = sortList(slow);
+    
+    
+//     return merge(l1, l2);
+//   }
   
-  ListNode merge(ListNode l1, ListNode l2) {
-    ListNode l = new ListNode(0), p = l;
+//   ListNode merge(ListNode l1, ListNode l2) {
+//     ListNode l = new ListNode(0), p = l;
     
-    while (l1 != null && l2 != null) {
-      if (l1.val < l2.val) {
-        p.next = l1;
-        l1 = l1.next;
-      } else {
-        p.next = l2;
-        l2 = l2.next;
-      }
-      p = p.next;
-    }
+//     while (l1 != null && l2 != null) {
+//       if (l1.val < l2.val) {
+//         p.next = l1;
+//         l1 = l1.next;
+//       } else {
+//         p.next = l2;
+//         l2 = l2.next;
+//       }
+//       p = p.next;
+//     }
     
-    if (l1 != null)
-      p.next = l1;
+//     if (l1 != null)
+//       p.next = l1;
     
-    if (l2 != null)
-      p.next = l2;
+//     if (l2 != null)
+//       p.next = l2;
     
-    return l.next;
-  }
+//     return l.next;
+//   }
 
-}
+// }
