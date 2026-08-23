dc957bff02032f6fea0cd6480c76d53e1791d657
Rehan <rehan.d.3011@gmail.com>
2026-08-23 17:12:22 +0530

    ✅ Find All Numbers Disappeared in an Array II | Medium | Java | Runtime: 93 ms (15.02%) | Memory: 175.5 MB | 2026-08-23

--- leetcode/medium/4031-find-all-numbers-disappeared-in-an-array-ii/solution.java
+++ leetcode/medium/4031-find-all-numbers-disappeared-in-an-array-ii/solution.java
@@ -2,36 +2,57 @@ import java.util.*;
 
 class Solution {
     public List<List<Integer>> findDisappearedNumbers(int[] nums, int lower, int upper) {
-        
-        Set<Integer> presentNumbers = new HashSet<>();
-        for (int num : nums) {
-            presentNumbers.add(num);
+        Set <Integer> hs = new HashSet<>();
+        List<List<Integer>> ls = new ArrayList<>();
+        int i = lower;
+        for(int j:nums){
+            hs.add(j);
         }
-
-        List<List<Integer>> result = new ArrayList<>();
-        int startMissing = -1;
-
-        
-        for (int i = lower; i <= upper; i++) {
-            if (!presentNumbers.contains(i)) {
-                
-                if (startMissing == -1) {
-                    startMissing = i;
-                }
-            } else {
-                
-                if (startMissing != -1) {
-                    result.add(Arrays.asList(startMissing, i - 1));
-                    startMissing = -1; 
-                }
+        while(i <= upper){
+            if(hs.contains(i)){
+                i++;
+                continue;
             }
+            int start = i;
+            while(i<=upper && !hs.contains(i)){
+                i++;
+            }
+            int high = i-1;
+            ls.add(Arrays.asList(start,high));
         }
+        return ls;
+    }
+    // public List<List<Integer>> findDisappearedNumbers(int[] nums, int lower, int upper) {
+        
+    //     Set<Integer> presentNumbers = new HashSet<>();
+    //     for (int num : nums) {
+    //         presentNumbers.add(num);
+    //     }
+
+    //     List<List<Integer>> result = new ArrayList<>();
+    //     int startMissing = -1;
+
+        
+    //     for (int i = lower; i <= upper; i++) {
+    //         if (!presentNumbers.contains(i)) {
+                
+    //             if (startMissing == -1) {
+    //                 startMissing = i;
+    //             }
+    //         } else {
+                
+    //             if (startMissing != -1) {
+    //                 result.add(Arrays.asList(startMissing, i - 1));
+    //                 startMissing = -1; 
+    //             }
+    //         }
+    //     }
 
        
-        if (startMissing != -1) {
-            result.add(Arrays.asList(startMissing, upper));
-        }
+    //     if (startMissing != -1) {
+    //         result.add(Arrays.asList(startMissing, upper));
+    //     }
 
-        return result;
-    }
+    //     return result;
+    // }
 }
