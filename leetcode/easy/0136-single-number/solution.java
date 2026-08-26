5b1526bc51c92b2f5f243e6d26965160f5673401
Rehan <rehan.d.3011@gmail.com>
2026-08-26 23:18:17 +0530

    ✅ Single Number | Easy | Java | Runtime: 11 ms (24.01%) | Memory: 48.4 MB | 2026-08-26

--- leetcode/easy/0136-single-number/solution.java
+++ leetcode/easy/0136-single-number/solution.java
@@ -1,13 +1,53 @@
 class Solution {
+    // public int singleNumber(int[] nums) {
+    //     Map<Integer,Integer> mp = new HashMap<>();
+    //     int n = nums.length;
+    //     for(int i:nums){
+    //         mp.put(i,mp.getOrDefault(i,0)+1);
+    //     }
+    //     for(Map.Entry<Integer,Integer> entry: mp.entrySet()){
+    //         if(entry.getValue()==1){
+    //             return entry.getKey();
+    //         }
+    //     }
+    //     return 0;
+    // }
+    // public int singleNumber(int[] nums) {
+    //     Arrays.sort(nums);
+    //     int n = nums[nums.length-1];
+    //     int arr[] = new int[n+1];
+
+    //     for(int i:nums){
+    //         arr[i]++;
+    //     }
+
+    //     for(int j:nums){
+    //         if(arr[j]==1){
+    //             return j; 
+    //         }
+    //     }
+    //      return 0;
+    // }
+
     public int singleNumber(int[] nums) {
-        Map<Integer,Integer> mp = new HashMap<>();
-        int n = nums.length;
-        for(int i:nums){
-            mp.put(i,mp.getOrDefault(i,0)+1);
+        // Step 1: Sort to easily find the min and max values
+        Arrays.sort(nums);
+        int min = nums[0];
+        int max = nums[nums.length - 1];
+        
+        // Step 2: Size the array to fit the entire range of numbers safely
+        int range = max - min + 1;
+        int arr[] = new int[range];
+
+        // Step 3: Shift indices by subtracting 'min' to handle negative numbers
+        for (int i : nums) {
+            arr[i - min]++;
         }
-        for(Map.Entry<Integer,Integer> entry: mp.entrySet()){
-            if(entry.getValue()==1){
-                return entry.getKey();
+
+        // Step 4: Find the element with a count of 1
+        for (int i : nums) {
+            if (arr[i - min] == 1) {
+                return i; 
             }
         }
         return 0;
