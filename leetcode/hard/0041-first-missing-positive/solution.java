560c677754a339bbe4172ad7e8256dd96ce7f155
Rehan <rehan.d.3011@gmail.com>
2026-08-29 14:36:48 +0530

    ✅ First Missing Positive | Hard | Java | Runtime: 17 ms (18.08%) | Memory: 93.2 MB | 2026-08-29

--- leetcode/hard/0041-first-missing-positive/solution.java
+++ leetcode/hard/0041-first-missing-positive/solution.java
@@ -1,17 +1,39 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
+        
+        Set <Integer> set = new HashSet<>();
+
         int n = nums.length;
-        Arrays.sort(nums);
-        int start = 1;
 
         for(int i:nums){
-            if(i>0&&i==start){
+            if(i>0){
+                set.add(i);
+            }
+        }
+        int start = 1;
+        for(int j:set){
+            if(set.contains(start)){
                 start++;
             }
-            else if(i>start){
+            else{
                 return start;
             }
         }
         return start;
+        
+        //solution 1
+        // int n = nums.length;
+        // Arrays.sort(nums);
+        // int start = 1;
+
+        // for(int i:nums){
+        //     if(i>0&&i==start){
+        //         start++;
+        //     }
+        //     else if(i>start){
+        //         return start;
+        //     }
+        // }
+        // return start;
     }
 }
