5af3adb302c83ba74ea13a6576a54cbadd2b584a
Rehan <rehan.d.3011@gmail.com>
2026-09-21 23:31:52 +0530

    ✅ Missing Number | Easy | Java | Runtime: 8 ms (16.18%) | Memory: 48.4 MB | 2026-09-21

--- leetcode/easy/0268-missing-number/solution.java
+++ leetcode/easy/0268-missing-number/solution.java
@@ -1,21 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
-
-        int n = nums.length;
-        int sum = (n*(n+1))/2;
-        int actualSum = 0;
-        for(int i=0; i<n; i++){
-            actualSum +=nums[i];
+        Arrays.sort(nums);
+    int i;
+        for( i=0; i<nums.length; i++){
+            if(nums[i]!=i){
+                return i;
+            }
         }
-        return -(actualSum-sum);
-        // int n = nums.length;
-        // Arrays.sort(nums);
-        // if(nums[n-1]!=n) return n;
-        // for(int i=0; i<n; i++){
-        //     if(nums[i]!=i){
-        //         return i;
-        //     }
-        // }
-        // return -1;
+        return i;
     }
 }
